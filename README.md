8bbe8a7a4524ed091801e77c5ce50765495781c6
paul cortes <paul.cortes@forteinnovation.mx>
2023-06-25 12:45:42 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+proyecto para profundizar en conceptos de maven
+este proyecto contiene a la clase persona y se
+instala como artifact en el repositorio local con
+mvn install, de esta manera el proyecto HelloWorldMaven
+puede usarlo en su POM. https://github.com/pcorqui/HelloWorldMaven
---
+++ src/main/java/Entity/Persona.java
@@ -0,0 +1,40 @@
+package Entity;
+
+public class Persona {
+    Integer Id;
+    String nombre;
+    String apellido;
+    String edad;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+}
